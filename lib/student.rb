a91e13f4e9b9c7971e6c3566a6d299c4e2af5bed
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-20 11:45:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,11 @@
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+    def initialize(name, grade, id = nil)
+      @name = name 
+      @grade = grade 
+      @id = id 
+    end 
 end
+
+# Remember, you can access your database connection anywhere in this class
+#  with DB[:conn] 
